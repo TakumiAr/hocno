60e2100ed40e654a05196792f98b8daeeffadd85
yuji hirai <yujihirai0420@gmail.com>
2019-09-13 20:05:34 +0900

    postとuserを結びつけ。ユーザーの詳細ページ作成。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_13_092613) do
+ActiveRecord::Schema.define(version: 2019_09_13_103630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,9 @@ ActiveRecord::Schema.define(version: 2019_09_13_092613) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id"
+    t.bigint "user_id"
     t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -50,4 +52,5 @@ ActiveRecord::Schema.define(version: 2019_09_13_092613) do
   end
 
   add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
