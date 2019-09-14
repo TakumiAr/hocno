7d567902390dbb066b93fc88ee72268647fe96d5
yuji hirai <yujihirai0420@gmail.com>
2019-09-14 17:22:19 +0900

    投稿にいいね数を表示。likesテーブルにindex追加。

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :likes
+  has_many :like_users, through: :likes, source: :user
   enum status: { 迷い中: 0, 購入: 1, 断念: 2 }
   mount_uploader :image, ImageUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_14_072431) do
+ActiveRecord::Schema.define(version: 2019_09_14_081908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_09_14_072431) do
     t.integer "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
