a5ded5ee749f5b5c96c236c3c8a588ed540605ad
yuji hirai <yujihirai0420@gmail.com>
2019-09-19 20:22:45 +0900

    userのimageカラムをiconに変更したことによりimageと記載していた箇所をiconに変更。

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -20,9 +20,7 @@ class ImageUploader < CarrierWave::Uploader::Base
   #
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
-  def default_url
-    "default.jpg"
-  end
+
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
